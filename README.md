e5df5af469fa8d84969a789038b201686446f164
hohav <hohav@fastmail.com>
2021-01-25 22:09:35 -0800

    replace HDF5 output format with new Peppi format

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ Build it with `cargo build --release` (requires nightly Rust).
 
 ## Usage
 
+Run `slp --help` for a complete list of options.
+
+### Examples
+
 Print the post-frame action state for each port (player) on the last frame of the game:
 
 ```bash
@@ -15,17 +19,47 @@ $ slp -nq frames[-1].ports[].leader.post.state game.slp
 ["14:WAIT","1:DEAD_LEFT"]
 ```
 
-Convert a replay to JSON (frame data is elided by default; use `-f` to override this):
+Convert a replay to JSON:
 
 ```bash
-$ slp -j game.slp
-{"start":{"slippi":{"version":[1,0,0]},"bitfield":[50,1,76],"is_teams":false,"item_spawn_frequency":-1,"self_destruct_score":-1,"stage":8,"timer":480,"item_spawn_bitfield":[255,255,255,255,255],"damage_ratio":1.0,"players":[{"port":"P1","character":9,"type":0,"stocks":4,"costume":3,"team":null,"handicap":9,"bitfield":192,"cpu_level":null,"offense_ratio":0.0,"defense_ratio":1.0,"model_scale":1.0,"ucf":{"dash_back":null,"shield_drop":null}},{"port":"P2","character":2,"type":1,"stocks":4,"costume":0,"team":null,"handicap":9,"bitfield":64,"cpu_level":1,"offense_ratio":0.0,"defense_ratio":1.0,"model_scale":1.0,"ucf":{"dash_back":null,"shield_drop":null}}],"random_seed":3803194226},"end":{"method":3},"metadata":{"date":"2018-06-22T07:52:59Z","duration":5209,"platform":"dolphin","players":[{"port":"P1","characters":{"18":5209}},{"port":"P2","characters":{"1":5209}}]}}
+$ slp -s game.slp | jq # `-s` to skip frame data; `jq` for pretty-printing
+{
+  "metadata": {
+    "date": "2018-06-22T07:52:59Z",
+    "duration": 5209,
+    "platform": "dolphin",
+    "players": [
+      {
+        "port": "P1",
+        "characters": {
+          "18": 5209
+        }
+      },
+      {
+        "port": "P2",
+        "characters": {
+          "1": 5209
+        }
+      }
+    ]
+  },
+  "start": {...},
+  "end": {...}
+}
 ```
 
-Convert a replay to [HDF5](https://www.hdfgroup.org/solutions/hdf5/):
+Convert a replay to Peppi format:
 
 ```bash
-$ slp -o hdf5 game.slp > game.hdf5
+$ slp -f peppi -o foo game.slp
 ```
 
-Run `slp --help` for more info.
+This creates a directory `foo` with the following files:
+```
+start.json
+end.json
+metadata.json
+frames.hdf5
+```
+
+`frames.hdf5` stores the game's frame data in columnar [HDF5](https://www.hdfgroup.org/solutions/hdf5/) format. It will be several times larger than the original .slp file, but significantly smaller when compressed.
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,81 @@
-use std::{fs, io};
+use std::{fs, io, path};
 use std::error::Error;
+use std::io::Write;
 
 use clap::{App, Arg};
 use jmespatch::ToJmespath;
+
+use peppi::game::Game;
 
 mod hdf5;
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
+#[derive(Copy, Clone)]
 enum Format {
-	Rust, Json, Hdf5
+	Rust, Json, Peppi
 }
 
 struct Opts {
-	path: String,
+	infile: String,
+	outfile: String,
 	format: Format,
-	frames: bool,
+	skip_frames: bool,
 	enum_names: bool,
 	query: Option<String>,
 }
 
-fn inspect<R: io::Read>(buf: &mut R, format: Format, query: Option<String>) -> Result<(), Box<dyn Error>> {
-	let game = peppi::game(buf)?;
-	if let Some(query) = &query {
-		let query = jmespatch::compile(query)?;
-		let jmes = game.to_jmespath()?;
-		let result = query.search(jmes)?;
-		println!("{}", serde_json::to_string(&result)?);
-	} else {
-		use Format::*;
-		match format {
-			Json => println!("{}", serde_json::to_string(&game)?),
-			Rust => println!("{:#?}", game),
-			Hdf5 => {
-				//FIXME: find a better approach than writing to a temp file
-				let mut f = tempfile::NamedTempFile::new()?;
-				hdf5::write(&game, f.path())?;
-				io::copy(&mut f, &mut io::stdout())?;
-			},
-		};
+fn write_peppi<P: AsRef<path::Path>>(game: &Game, dir: P, skip_frames: bool) -> Result<(), Box<dyn Error>> {
+	let dir = dir.as_ref();
+	fs::create_dir_all(dir)?;
+	fs::write(dir.join("metadata.json"), serde_json::to_string(&game.metadata)?)?;
+	fs::write(dir.join("start.json"), serde_json::to_string(&game.start)?)?;
+	fs::write(dir.join("end.json"), serde_json::to_string(&game.end)?)?;
+	if !skip_frames {
+		hdf5::write(game, dir.join("frames.hdf5"))?;
 	}
 	Ok(())
+}
+
+fn write_json<W: Write>(game: &Game, mut out: W) -> Result<(), Box<dyn Error>> {
+	writeln!(out, "{}", serde_json::to_string(game)?)?;
+	Ok(())
+}
+
+fn write_rust<W: Write>(game: &Game, mut out: W) -> io::Result<()> {
+	writeln!(out, "{:#?}", game)
+}
+
+fn write_query<W: Write>(game: &Game, query: &str, mut out: W) -> Result<(), Box<dyn Error>> {
+	let query = jmespatch::compile(query)?;
+	let jmes = game.to_jmespath()?;
+	let result = query.search(jmes)?;
+	writeln!(out, "{}", serde_json::to_string(&result)?)?;
+	Ok(())
+}
+
+fn write<W: Write>(game: &Game, out: W, format: Format, query: Option<&String>) -> Result<(), Box<dyn Error>> {
+	use Format::*;
+	match (format, query) {
+		(Json, Some(q)) => write_query(game, q, out)?,
+		(_, Some(_)) => Err("queries only support JSON output")?,
+		(Json, _) => write_json(game, out)?,
+		(Rust, _) => write_rust(game, out)?,
+		_ => unimplemented!(),
+	}
+	Ok(())
+}
+
+fn inspect<R: io::Read>(mut buf: R, opts: &Opts) -> Result<(), Box<dyn Error>> {
+	let game = peppi::game(&mut buf)?;
+	use Format::*;
+	match (opts.format, opts.outfile.as_str()) {
+		(Peppi, "-") => Err("cannot output Peppi to STDOUT")?,
+		(Peppi, o) => write_peppi(&game, o, opts.skip_frames),
+		(format, "-") => write(&game, io::stdout(), format, opts.query.as_ref()),
+		(format, s) => write(&game, fs::File::create(s)?, format, opts.query.as_ref()),
+	}
 }
 
 fn parse_opts() -> Result<Opts, String> {
@@ -49,21 +83,17 @@ fn parse_opts() -> Result<Opts, String> {
 		.version("0.1")
 		.author("melkor <hohav@fastmail.com>")
 		.about("Inspector for Slippi SSBM replay files")
+		.arg(Arg::with_name("outfile")
+			 .help("Output path")
+			 .short("o")
+			 .default_value("-"))
 		.arg(Arg::with_name("format")
 			 .help("Output format")
-			 .short("o")
-			 .possible_values(&["json", "rust", "hdf5"])
-			 .default_value("rust"))
-		.arg(Arg::with_name("json")
-			.help("Output as JSON (same as `-o json`)")
-			.short("j")
-			.long("json"))
-		.arg(Arg::with_name("frames")
-			.help("Output frame data")
-			.short("f")
-			.long("frames"))
-		.arg(Arg::with_name("QUERY")
-			.help("Print a subset of parsed data (JMESPath syntax; implies `-j`)")
+			 .short("f")
+			 .possible_values(&["json", "peppi", "rust"])
+			 .default_value("json"))
+		.arg(Arg::with_name("query")
+			.help("Print a subset of data as JSON (JMESPath syntax)")
 			.short("q")
 			.long("query")
 			.takes_value(true))
@@ -71,30 +101,35 @@ fn parse_opts() -> Result<Opts, String> {
 			.help("Append names for known constants")
 			.short("n")
 			.long("names"))
-		.arg(Arg::with_name("FILE")
+		.arg(Arg::with_name("skip-frames")
+			.help("Don't output frame data")
+			.short("s")
+			.long("skip-frames"))
+		.arg(Arg::with_name("game.slp")
 			.help("Replay file to parse (`-` for STDIN)")
 			.index(1))
 		.get_matches();
 
-	let path = matches.value_of("FILE").unwrap_or("-");
-	let format = if matches.is_present("json") {
-		Format::Json
-	} else {
+	let infile = matches.value_of("game.slp").unwrap_or("-");
+	let outfile = matches.value_of("outfile").unwrap();
+
+	let format = {
 		use Format::*;
 		match matches.value_of("format").unwrap() {
 			"json" => Json,
 			"rust" => Rust,
-			"hdf5" => Hdf5,
-			o => Err(format!("unsupported output format: {}", o))?,
+			"peppi" => Peppi,
+			_ => unimplemented!(),
 		}
 	};
 
 	Ok(Opts {
-		path: path.to_string(),
+		infile: infile.to_string(),
+		outfile: outfile.to_string(),
 		format: format,
-		frames: matches.is_present("frames") || matches.is_present("QUERY"),
+		skip_frames: matches.is_present("skip-frames"),
 		enum_names: matches.is_present("names"),
-		query: matches.value_of("QUERY").map(|q| q.to_string()),
+		query: matches.value_of("query").map(|q| q.to_string()),
 	})
 }
 
@@ -104,15 +139,13 @@ pub fn main() -> Result<(), Box<dyn Error>> {
 	let opts = parse_opts()?;
 	unsafe {
 		peppi::CONFIG = peppi::Config {
-			frames: opts.frames,
+			skip_frames: opts.skip_frames,
 			enum_names: opts.enum_names,
 		}
 	};
 
-	if opts.path == "-" {
-		inspect(&mut io::stdin(), opts.format, opts.query)
-	} else {
-		let mut buf = io::BufReader::new(fs::File::open(opts.path)?);
-		inspect(&mut buf, opts.format, opts.query)
+	match opts.infile.as_str() {
+		"-" => inspect(io::stdin(), &opts),
+		path => inspect(io::BufReader::new(fs::File::open(path)?), &opts),
 	}
 }
